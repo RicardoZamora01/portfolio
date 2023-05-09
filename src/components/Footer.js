dce01fd42c79aa18bc486485b53e4c4fd80e5179
Ricardo Zamora <rzgarcia27@gmail.com>
2023-05-08 22:36:48 -0400

    responsive footer update

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+// import logo from "../assets/rz-bird-logo.png";
+import logo from "../assets/bird_logo.svg";
+import linkedin from "../assets/linkedin-64.svg";
+import github from "../assets/github-64.svg";
+
+const Footer = () => {
+    return (
+        <footer className="footer items-center p-4 bg-neutral text-neutral-content gap-4 sm:gap-2 px-16">
+            <div className="justify-self-center md:justify-self-start items-center grid-flow-col">
+                <img src={logo} alt="rz bird logo" className="w-12"/>
+                <p>Copyright © 2023 - All right reserved</p>
+            </div> 
+            <div className="grid-flow-col gap-4 justify-self-center md:justify-self-end">
+                <a href={"https://www.linkedin.com/in/zamora-ricardo/"}>
+                    <img src={linkedin} alt="linked in logo" className="w-12"/>
+                </a>
+                <a href={"https://github.com/RicardoZamora01"}>
+                    <img src={github} alt="github logo" className="w-12"/>
+                </a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
+
