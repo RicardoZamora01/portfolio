1ea04df21aa9b224d015eb54bfdf652087d19021
Ricardo Zamora <rzgarcia27@gmail.com>
2023-05-13 23:28:05 -0400

    navbar scroll into view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,11 @@
 import logo from "../assets/bird_logo.svg"
 
-
 const Navbar = () => {
+    const handleClick = (idName) => {
+        const element = document.getElementById(idName);
+        element.scrollIntoView({behavior: 'smooth'});
+    }
+
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
@@ -9,15 +13,14 @@ const Navbar = () => {
             </div>
             <div className="flex max-w-max sm:flex-none">
                 <ul className="menu menu-horizontal px-1 hidden sm:flex">
-                    <li key="Home"><a href="#/">Home</a></li>
-                    <li key="About"><a href ="#/">About</a></li>
-                    <li key="Projects"><a href="#/">Projects</a></li>
-                    <li key="Contact"><a href ="#/">Contact</a></li>
+                    <li key="Home"><button onClick={() => handleClick("home")}>Home</button></li>
+                    <li key="About"><button onClick={() => handleClick("about-container")}>About</button></li>
+                    <li key="Projects"><button onClick={() => handleClick("project-container")}>Projects</button></li>
+                    <li key="Contact"><button onClick={() => handleClick("contact-container")}>Contact</button></li>
                 </ul>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="m-1 w-6 h-6 visible sm:invisible">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
                 </svg>
-
             </div>
         </div>
     )
